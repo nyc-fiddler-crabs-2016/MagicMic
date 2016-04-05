d52af6a6acca3a7128222efce554100900b1e908
Matthew Oppenheimer & Joe Plonsker & gary tso <pair+mattopps.plonsker.superboyblue@devbootcamp.com>
2016-04-04 20:13:49 -0400

    add host to application.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,10 @@ default from: 'notifications@example.com'
 
   def welcome_email(user)
     @user = user
+    email_with_name = %("#{@user.name}"<#{@user.email}>)
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to MagicMic')
+    mail(to: email_with_name,
+      subject: 'Welcome to MagicMic')
   end
 
 
